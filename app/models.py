9107632819054ef7029502ef6a0ad3bf65dd3163
GeGe-K <gloriagivondo@gmail.com>
2018-11-27 09:27:58 +0300

    edited the navbar.

--- app/models.py
+++ app/models.py
@@ -87,4 +87,10 @@ class Comment(db.Model):
         db.session.add(self)
         db.session.commit()
 
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(), unique = True)
+
     
