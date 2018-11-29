ec9d57c7637d1d0e8fcdc8e8dc053db882b1bd24
GeGe-K <gloriagivondo@gmail.com>
2018-11-29 05:01:07 +0300

    filefield defined.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SelectField,SubmitField
+from wtforms import StringField,TextAreaField,FileField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 
 class CommentForm(FlaskForm):
